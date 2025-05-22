e66dc6f8d24c81f537158c1c4afbc36b7c139801
Isaque Encarnação <isaque.daniel.encarnacao@gmail.com>
2025-05-23 00:38:08 +0100

    Já é possível remover dos favoritos e adicionar ao carrinho

--- frontend/public/Favoritos.jsx
+++ frontend/public/Favoritos.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import ModalErro from "../componentes/ModalErro";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../componentes/Navbar";
 import SubNavbar from "../componentes/SubNavbar";
 import styles from "../css/Favoritos.module.css";
 
 function Favoritos() {
   const [favoritos, setFavoritos] = useState([]);
+  const [carrinho, setCarrinho] = useState(null);
+  const [mensagem, setMensagem] = useState("");
+  const [produtoModal, setProdutoModal] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
   const user = JSON.parse(localStorage.getItem("user"));
+
+  const navigate = useNavigate();
 
   const handleLogout = () => {
       localStorage.removeItem('user');
-      Navigate('/');
+      navigate('/');
     };
 
   useEffect(() => {
@@ -29,6 +36,77 @@ function Favoritos() {
         .catch((err) => console.error("Erro ao carregar favoritos", err));
     }
   }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`http://localhost:3001/api/carrinhos/${user.id}`)
+        .then((res) => setCarrinho(res.data))
+        .catch((err) => console.error("Erro ao carregar carrinho", err));
+    }
+  }, [user]);
+
+  const handleRemoverFavorito = (id) => {
+    axios
+      .delete(`http://localhost:3001/api/favoritos/remover`, {
+        data: { id_utilizador: user.id, id_produto: id },
+      })
+      .then(() => {
+        setFavoritos((prevFavoritos) =>
+          prevFavoritos.filter((produto) => produto.id !== id)
+        );
+      })
+      .catch((err) => console.error("Erro ao remover favorito", err));
+  };
+
+  const handleAdicionarAoCarrinho = async (produto, quantidadeProduto = 1) => {
+    if (!user) {
+      setMensagem("Faça login para adicionar ao carrinho.");
+      setOpenModal(true);
+      return;
+    }
+
+    if (!carrinho) {
+      setMensagem("Carrinho não encontrado.");
+      setOpenModal(true);
+      return;
+    }
+
+    if (!produto) {
+      setMensagem("Produto não encontrado.");
+      setOpenModal(true);
+      return;
+    }
+
+    setProdutoModal(produto.nome);
+
+    try {
+      await axios.post("http://localhost:3001/api/carrinhos/adicionar", {
+        id_carrinho: carrinho.id,
+        id_produto: produto.id,
+        quantidade: quantidadeProduto,
+        preco: produto.preco,
+      });
+
+      setMensagem("Produto adicionado ao carrinho.");
+      setOpenModal(true);
+
+      setCarrinho((prevCarrinho) => ({
+        ...prevCarrinho,
+        total: prevCarrinho.total + quantidadeProduto * produto.preco,
+      }));
+
+      setTimeout(() => setOpenModal(false), 1500);
+    } catch (error) {
+      console.error("Erro ao adicionar ao carrinho", error);
+      setMensagem("Erro ao adicionar ao carrinho.");
+      setOpenModal(true);
+    }
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+  };
 
   return (
   <>
@@ -57,7 +135,7 @@ function Favoritos() {
               <h3>{produto.nome}</h3>
               <p>€{produto.preco}</p>
               <div style={{ marginTop: "auto", textAlign: "center" }}>
-                <button onClick={() => handleAdicionarAoCarrinho(produto.id)}>
+                <button onClick={() => handleAdicionarAoCarrinho(produto)}>
                   Adicionar ao carrinho
                 </button>
               </div>
@@ -66,6 +144,9 @@ function Favoritos() {
         </div>
       )}
     </div>
+    {openModal && (
+          <ModalErro mensagem={mensagem} onClose={closeModal} produtos={produtoModal} />
+        )}  
     </>
   );
 }
