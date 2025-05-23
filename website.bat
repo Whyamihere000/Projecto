8cc76b7e6ae6fa0a8051ef2dee62d5183ffa2c8f
Isaque Encarnação <isaque.daniel.encarnacao@gmail.com>
2025-05-23 13:50:22 +0100

    agora é possível aceder em qualquer computador

--- website.bat
+++ website.bat
@@ -1,19 +1,37 @@
+@REM @echo off
+
+@REM REM === Iniciar backend ===
+@REM if exist "C:\Users\Isaque\Desktop\PAP\Projecto\backend" (
+@REM     start "Backend" cmd /K "cd /d C:\Users\Isaque\Documents\GitHub\Projecto\backend && node server.js"
+@REM     start "Backend" cmd /K "cd /d C:\Users\Isaque\Desktop\PAP\Projecto\backend && node server.js"
+@REM ) else if exist "C:\Users\migue\Documents\GitHub\Projecto\backend" (
+@REM     start "Backend" cmd /K "cd /d C:\Users\migue\Documents\GitHub\Projecto\backend && node server.js"
+@REM )
+
+@REM REM === Iniciar frontend ===
+@REM if exist "C:\Users\Isaque\Desktop\PAP\Projecto" (
+@REM     start "Frontend" cmd /K "cd /d C:\Users\Isaque\Documents\GitHub\Projecto && npm run dev"
+@REM ) else if exist "C:\Users\migue\Documents\GitHub\Projecto" (
+@REM     start "Frontend" cmd /K "cd /d C:\Users\migue\Documents\GitHub\Projecto && npm run dev"
+@REM )
+
+@REM REM === Aguardar 2 segundos para o frontend arrancar ===
+@REM ping -n 3 127.0.0.1 >nul
+
+@REM REM === Abrir o browser ===
+@REM start "" "http://localhost:5173/"
+
+
 @echo off
+cd /d "%~dp0"
 
 REM === Iniciar backend ===
-if exist "C:\Users\Isaque\Desktop\PAP\Projecto\backend" (
-    start "Backend" cmd /K "cd /d C:\Users\Isaque\Documents\GitHub\Projecto\backend && node server.js"
-    start "Backend" cmd /K "cd /d C:\Users\Isaque\Desktop\PAP\Projecto\backend && node server.js"
-) else if exist "C:\Users\migue\Documents\GitHub\Projecto\backend" (
-    start "Backend" cmd /K "cd /d C:\Users\migue\Documents\GitHub\Projecto\backend && node server.js"
+if exist "backend" (
+    start "Backend" cmd /K "cd /d %cd%\backend && node server.js"
 )
 
 REM === Iniciar frontend ===
-if exist "C:\Users\Isaque\Desktop\PAP\Projecto" (
-    start "Frontend" cmd /K "cd /d C:\Users\Isaque\Documents\GitHub\Projecto && npm run dev"
-) else if exist "C:\Users\migue\Documents\GitHub\Projecto" (
-    start "Frontend" cmd /K "cd /d C:\Users\migue\Documents\GitHub\Projecto && npm run dev"
-)
+start "Frontend" cmd /K "cd /d %cd% && npm run dev"
 
 REM === Aguardar 2 segundos para o frontend arrancar ===
 ping -n 3 127.0.0.1 >nul
